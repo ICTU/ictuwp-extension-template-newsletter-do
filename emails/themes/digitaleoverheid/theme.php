a298af6f7b1b48e0c1c7e088350d06abbda36ac1
Paul van Buuren <paul@wbvb.nl>
2024-03-21 21:20:17 +0100

    code formatting

--- emails/themes/digitaleoverheid/theme.php
+++ emails/themes/digitaleoverheid/theme.php
@@ -1004,32 +1004,33 @@ function maak_event( $eventobject, $asseturl, $theme_options ) {
 										?>
 										<?php // START (1) check if any posts are available
 										if ( $post_counter > 0 ) {
-										?>
+											?>
 
 
-										<table role="presentation" border="0" cellpadding="0" cellspacing="0"
-											   width="100%"
-											   class="mcnDividerBlock" style="min-width:100%;">
-											<tbody class="mcnDividerBlockOuter">
-											<tr>
-												<td class="mcnDividerBlockInner" style="min-width:100%; padding:9px;">
-													<table role="presentation" class="mcnDividerContent" border="0"
-														   cellpadding="0"
-														   cellspacing="0" width="100%"
-														   style="min-width: 100%;border-top: 1px solid #EAEAEA;">
-														<tbody>
-														<tr>
-															<td><span></span></td>
-														</tr>
-														</tbody>
-													</table>
-												</td>
-											</tr>
-											</tbody>
-										</table>
+											<table role="presentation" border="0" cellpadding="0" cellspacing="0"
+												   width="100%"
+												   class="mcnDividerBlock" style="min-width:100%;">
+												<tbody class="mcnDividerBlockOuter">
+												<tr>
+													<td class="mcnDividerBlockInner"
+														style="min-width:100%; padding:9px;">
+														<table role="presentation" class="mcnDividerContent" border="0"
+															   cellpadding="0"
+															   cellspacing="0" width="100%"
+															   style="min-width: 100%;border-top: 1px solid #EAEAEA;">
+															<tbody>
+															<tr>
+																<td><span></span></td>
+															</tr>
+															</tbody>
+														</table>
+													</td>
+												</tr>
+												</tbody>
+											</table>
 											<?php // END (1) check if any posts are available
-}
-?>
+										}
+										?>
 
 										<table role="presentation" border="0" cellpadding="0" cellspacing="0"
 											   width="100%"
@@ -1049,7 +1050,7 @@ function maak_event( $eventobject, $asseturl, $theme_options ) {
 													<![endif]-->
 													<?php // START (2) check if any posts are available
 													if ( $post_counter > 0 ) {
-?>
+													?>
 
 													<table role="presentation" align="left" border="0" cellpadding="0"
 														   cellspacing="0"
@@ -1075,8 +1076,8 @@ function maak_event( $eventobject, $asseturl, $theme_options ) {
 													</tr>
 													</table>
 													<?php // END (2) check if any posts are available
-}
-?>
+													}
+													?>
 
 													<![endif]--></td>
 											</tr>
@@ -1168,7 +1169,7 @@ function maak_event( $eventobject, $asseturl, $theme_options ) {
 					?>
 
 					<tr>
-					<td align="center" valign="top" id="templateLowerBody">
+						<td align="center" valign="top" id="templateLowerBody">
 
 							<table role="presentation" align="center" border="0" cellpadding="0" cellspacing="0"
 								   width="100%"
